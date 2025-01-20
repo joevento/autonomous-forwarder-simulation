5253455cbbda88b978465bf1f16afb3901e36679
joevento <joe.te.vento@gmail.com>
2025-01-21 01:31:51 +0200

    basic rmpflow setup for the crane

--- extension/Autonomous_forwarder_python/robot.py
+++ extension/Autonomous_forwarder_python/robot.py
@@ -1,13 +1,19 @@
 import numpy as np
 import math
-import omni.usd
+import os
+import omni.isaac.core.utils.stage as stage_utils
+import omni.isaac.core.utils.prims as prim_utils
 
+from omni.isaac.dynamic_control import _dynamic_control
+from omni.isaac.core.prims import XFormPrim
 from scipy.interpolate import splprep, splev
 from omni.isaac.core.articulations import Articulation, ArticulationView
 from omni.isaac.core.utils.types import ArticulationAction
-from pxr import Sdf, Usd, UsdGeom
+from omni.isaac.motion_generation import RmpFlow, ArticulationMotionPolicy
+from omni.isaac.core.utils.numpy.rotations import euler_angles_to_quats
+from omni.isaac.core.utils.nucleus import get_assets_root_path
 
-from .global_variables import WHEEL_INDICES, MIDDLE_JOINT
+from . import global_variables
 
 # for waypoint debug
 from omni.isaac.core.objects import VisualSphere
@@ -34,9 +40,72 @@ class SplineInterpolator:
 class RobotHandler:
     def __init__(self):
         self._articulation = Articulation(prim_path="/forwarder/front", name="Robot")
+        self._articulation.initialize()
+        #global
+        global_variables.articulation = self._articulation
+        global_variables.articulation.initialize()
+
         self._articulation_view = ArticulationView(prim_paths_expr="/forwarder/front")
         self._articulation_view.initialize()
+        global_variables.articulation_view = self._articulation_view
 
+        self.world = global_variables.world_instance
+        print(self.world.scene)
+        print(self.world.scene.get_object(name="forwarder").get_articulation_controller())
+        self._articulation_controller = self.world.scene.get_object("forwarder").get_articulation_controller()
+        self._articulation_controller.initialize(global_variables.articulation_view)
+        #global
+        global_variables.articulation_controller = self._articulation_controller
+        global_variables.articulation_controller.initialize(self._articulation_view)
+
+        # rmpflow
+        # TODO: Make all the paths dynamic
+        extension_path = "H:/autonomous_forwarder/extension/Autonomous_forwarder_python"
+
+        rmp_config_dir = os.path.join(extension_path, "motion_policy_configs")
+
+        print(rmp_config_dir)
+        # init rmpflow object
+        self._rmpflow = RmpFlow(
+            robot_description_path= "H:/autonomous_forwarder/extension/Autonomous_forwarder_python/motion_policy_configs/crane_RDF.yaml",
+            urdf_path= "H:/autonomous_forwarder/extension/Autonomous_forwarder_python/motion_policy_configs/dev_forwarder_arm.urdf",
+            rmpflow_config_path= "H:/autonomous_forwarder/extension/Autonomous_forwarder_python/motion_policy_configs/crane_rmpflow_common.yaml",
+            end_effector_frame_name= "arm_end_effector",
+            maximum_substep_size=0.00334
+        )
+        # global
+        global_variables.rmpflow = self._rmpflow
+        self._articulation_rmpflow = ArticulationMotionPolicy(self._articulation, self._rmpflow)
+        #global
+        global_variables.articulation_rmpflow = self._articulation_rmpflow
+        
+        # TargetPos
+        stage_utils.add_reference_to_stage(get_assets_root_path() + "/Isaac/Props/UIElements/frame_prim.usd", "/World/target")
+        self._arm_target = XFormPrim("/World/target", scale=[.04, .04, .04])
+        self._arm_target.set_world_pose(np.array([.5,0,.7]),euler_angles_to_quats([0,np.pi,0]))
+        # global
+        global_variables.arm_target = self._arm_target
+
+        # https://docs.omniverse.nvidia.com/isaacsim/latest/advanced_tutorials/tutorial_motion_generation_rmpflow.html#generating-motions-with-an-rmpflow-instance
+
+    def move_arm(self, step):
+        target_pos, target_orient = self._arm_target.get_world_pose()
+        
+        self._rmpflow.set_end_effector_target(
+            target_pos, target_orient
+        )
+
+        # Track any movements of the cube obstacle
+        self._rmpflow.update_world()
+
+        #Track any movements of the robot base
+        robot_base_translation,robot_base_orientation = self._articulation.get_world_pose()
+        self._rmpflow.set_robot_base_pose(robot_base_translation,robot_base_orientation)
+        action = self._articulation_rmpflow.get_next_articulation_action(step)
+        self._articulation_controller.apply_action(action)
+        yield()
+
+    # Driving stuffs
     def drive_spline(self, speed, waypoints, accuracy=0.01, wp_xform=None):
         interpolator = SplineInterpolator(waypoints, k=3)
         path_points = interpolator.interpolate()
@@ -57,7 +126,7 @@ class RobotHandler:
         self._articulation_view.apply_action(
             ArticulationAction(
                 joint_velocities=np.array(speed),
-                joint_indices=np.array(WHEEL_INDICES)
+                joint_indices=np.array(global_variables.WHEEL_INDICES)
             )
         )
         position, rotation = self._articulation.get_world_pose()
@@ -73,7 +142,7 @@ class RobotHandler:
         self._articulation_view.apply_action(
             ArticulationAction(
                 joint_velocities=np.array([0]),
-                joint_indices=np.array(WHEEL_INDICES)
+                joint_indices=np.array(global_variables.WHEEL_INDICES)
             )
         )
         return True
@@ -102,7 +171,7 @@ class RobotHandler:
             self._articulation_view.apply_action(
                 ArticulationAction(
                     joint_positions=np.array(z_component),
-                    joint_indices=np.array(MIDDLE_JOINT)
+                    joint_indices=np.array(global_variables.MIDDLE_JOINT)
                 )
             )
             yield ()
